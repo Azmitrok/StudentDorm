0b7865ae544799b3a37f90e4d7647190d075f5e7
azmitrok <azmitrok@gmail.com>
2019-03-25 16:38:33 +0300

    colors by gender

--- Bookings.Web/ClientApp/src/app/bookings/bookings.component.ts
+++ Bookings.Web/ClientApp/src/app/bookings/bookings.component.ts
@@ -10,9 +10,10 @@ import { Room } from '../free-rooms/free-rooms.component';
 export class BookingsComponent {
   public bookings: Booking[];
   public filteredBookings: Booking[];
-  public appointments: Appointment[];
+  //public appointments: Appointment[];
   public currentDate: Date = new Date(2019, 2, 22);
   public allRooms: Room[];
+  public resourcesData: Resource[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
 
@@ -22,12 +23,14 @@ export class BookingsComponent {
 
     http.get<Booking[]>(baseUrl + 'api/Bookings/Index').subscribe(result => {
       
-      this.bookings = this.datesConvert(result);
+      this.bookings = this.prepareBookings(result);
       this.filteredBookings = this.bookings;
+      
       
     }, error => console.error(error));
 
-    this.appointments = this.getAppointments();
+    //this.appointments = this.getAppointments();
+    this.resourcesData = this.getResources();
     
   }
 
@@ -37,10 +40,11 @@ export class BookingsComponent {
         
   }
 
-  private datesConvert(bookings: Booking[]) {
+  private prepareBookings(bookings: Booking[]) {
     for (let booking of bookings) {
       booking.startDateObj = new Date(booking.startDate);
       booking.endDateObj = new Date(booking.endDate);
+      booking.scheduleText = "Room: " + booking.room.number + " ( " + booking.usedPlaces + " )"
     }
 
     return bookings;
@@ -63,9 +67,31 @@ export class BookingsComponent {
     return appointments;
   }
 
+  private getResources() {
+    let resourcesData: Resource[] = [
+      {
+        text: "Male",
+        id: 0,
+        color: "#1D69D2"
+      }, {
+        text: "Female",
+        id: 1,
+        color: "#DC5084"
+      }
+    ]
+
+    return resourcesData;
+  }
+
   
 
   
+}
+
+export class Resource {
+  text: string;
+  id: number;
+  color: string;
 }
 
 export class Appointment {
@@ -78,13 +104,15 @@ export class Appointment {
 export class Booking {
   id: number;
   roomId: number;
+  room: Room;
   startDate: string;
   endDate: string;
   startDateObj: Date;
   endDateObj: Date;
-  usedPalces: number;
+  usedPlaces: number;
  // gender: string;
-  gender: Gender;  
+  gender: Gender;
+  scheduleText: string;
 }
 
 export enum Gender {
